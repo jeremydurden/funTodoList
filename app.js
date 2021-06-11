e55dc9d40c338440be1d22e2695a1ba54a80c417
Jeremy Durden <jeremydurden@gmail.com>
2021-06-11 16:53:46 -0700

    adding JavaScript to create and append todo list items with generic text

---
+++ app.js
@@ -0,0 +1,37 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+
+//Functions
+function addTodo(){
+    //prevents page refresh on form submission
+    event.preventDefault();
+    
+    //todo DIV
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    //create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = 'hey';
+    newTodo.classList.add('todo-item')
+    //
+    todoDiv.appendChild(newTodo)
+    //check button
+    const checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkButton.classList.add('complete-btn');
+    todoDiv.appendChild(checkButton)
+    //trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('complete-btn');
+    todoDiv.appendChild(trashButton)
+    //append to list
+    todoList.appendChild(todoDiv);
+    
+}
