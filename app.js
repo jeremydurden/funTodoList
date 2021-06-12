6bf7042cc5060d1e625faa961261f1fe351472b9
Jeremy Durden <jeremydurden@gmail.com>
2021-06-11 18:05:57 -0700

    adding transition for deleting todo list item

--- app.js
+++ app.js
@@ -43,7 +43,12 @@ function deleteCheck(e){
     //delete todo
     if (item.classList[0] === 'trash-btn'){
         const todo = item.parentElement;
-        todo.remove();
+        //Animation
+        todo.classList.add('fall')
+        todo.addEventListener('transitionend', function(){
+            todo.remove();
+        })
+        
     }
     //Check Mark
     if (item.classList[0] === 'complete-btn'){
